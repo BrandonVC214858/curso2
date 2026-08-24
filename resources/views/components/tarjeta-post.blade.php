19d8720030ebfb88b411af7f73bad89d8d3d1fea
Brandon <brandon.venegas26@gmail.com>
2026-08-24 15:56:56 -0600

    Implement Post management features: create, edit, update, and delete functionality; update routes and views accordingly.

--- resources/views/components/tarjeta-post.blade.php
+++ resources/views/components/tarjeta-post.blade.php
@@ -13,5 +13,7 @@
     @if ($post->es_nuevo)
         <span class="inline-block bg-green-100 text-green-800 text-xs font-semibold px-2 py-1 rounded-full mb-2">NUEVO</span>
     @endif
+    <br>
+    <a href="{{ route('avisos.edit', $post) }}" class="text-blue-700 text-sm font-semibold hover:underline mt-4 inline-block">Editar</a>
 
 </article>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    $posts = Post::publicados()->with('categoria')->latest()->get();
+Route::get('/', [PostController::class, 'index'])->name('avisos.index');
 
-    return view('portada', ['posts' => $posts]);
-});
+Route::get('/avisos/crear', [PostController::class, 'create'])->name('avisos.create');
+Route::post('/avisos', [PostController::class, 'store'])->name('avisos.store');
+Route::get('/avisos/{post}/editar', [PostController::class, 'edit'])->name('avisos.edit');
+Route::put('/avisos/{post}', [PostController::class, 'update'])->name('avisos.update');
+Route::delete('/avisos/{post}', [PostController::class, 'destroy'])->name('avisos.destroy');
 
 Route::get('/contacto', fn () => view('contacto'));
