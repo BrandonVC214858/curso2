c3788eb4a2e76544f6d0e68cc60a1040cd4f9c35
Brandon <brandon.venegas26@gmail.com>
2026-08-25 15:35:15 -0600

    Add auth funcionalities

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriaSeeder::class,
             PostSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
