005cc4bc5cd3dead265cc7dafaba979c5b71b0e2
Brandon <brandon.venegas26@gmail.com>
2026-09-22 16:32:00 -0600

    sesion9 - ACT1&2

--- api-django/avisos/views.py
+++ api-django/avisos/views.py
@@ -1,3 +1,35 @@
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from avisos.permissions import EsAutorOAdmin
+from rest_framework.response import Response
 
-# Aqui escribes AvisoViewSet y la vista yo (guia 02, pasos 2 y 4).
+
+from .models import Aviso
+from .serializers import AvisoSerializer
+
+
+class AvisoViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, EsAutorOAdmin]
+    queryset = Aviso.objects.filter(publicado=True).select_related("categoria", "autor")
+    serializer_class = AvisoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(autor=self.request.user)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = self.request.query_params.get("categoria")
+        if categoria:
+            qs = qs.filter(categoria_id=categoria)
+        return qs
+
+    
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def yo(request):
+    return Response({
+        "id": request.user.id,
+        "nombre": request.user.username,
+        "rol": "admin" if request.user.is_staff else "autor",
+    })
+
--- api-django/config/urls.py
+++ api-django/config/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
-# En la guia 02 agregas aqui el router de DRF, /api/token y /api/yo.
+from avisos.views import AvisoViewSet, yo
+
+router = DefaultRouter()
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Entrar y salir de la API navegable de DRF, la que se ve en el navegador.
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/token", obtain_auth_token),
+    path("api/avisos/", AvisoViewSet.as_view({"get": "list", "post": "create"})),
+    path("api/avisos/<int:pk>/", AvisoViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
+    path("api/yo/", yo),
 ]
